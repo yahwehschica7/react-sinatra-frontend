a837538b914f461288dea9d1b1c2d02e6c53188b
Jessica Nelson <yahwehschica@yahoo.com>
2022-06-02 10:19:47 -0400

    set up initial fetch in home

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,11 +7,17 @@ const Home = () => {
   const [categories, setCategories] = useState("")
   const [books, setBooks] = useState([])
 
+  useEffect(() => {
+    fetch("http://localhost:9292/books")
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+  }, [])
+
   return (
     <div>
       <h2 style={{backgroundColor: "lightblue"}}>Books on your TBR:</h2>
         
-        <div class="dropdown">
+        <div className="dropdown">
           <button class="dropbtn" style={{backgroundColor: "lightcyan"}}>ALL</button>
           
     </div>
