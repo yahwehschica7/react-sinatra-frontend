08e2f02ab149b17921da2317b574b1beed31017b
Jessica Nelson <yahwehschica@yahoo.com>
2022-06-06 12:51:26 -0400

    designed book card

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -3,7 +3,17 @@ import React from 'react'
 const BookCard = ({books}) => {
   return (
     <div>
-        <h2>List of books</h2>
+        <h2>{books.map(book => 
+            <h3>
+                {book.title}
+                <div>by: {book.author}</div>
+                    <div>My Thoughts:</div> 
+                        <i>
+                        <div>{book.comment}</div> 
+                        </i>
+            </h3>
+            )}
+        </h2>
     </div>
   )
 }
