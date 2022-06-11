535d45065b7ee2573af85b05fef7ec675f0de343
Jessica Nelson <yahwehschica@yahoo.com>
2022-06-11 12:43:23 -0400

    import useNavigate for post fetch in newcategory

--- src/components/NewCategory.js
+++ src/components/NewCategory.js
@@ -1,15 +1,43 @@
-import React from 'react'
+import React, {useState} from 'react'
+import {useNavigate} from "react-router-dom"
 
 const NewCategory = () => {
+
+  const [name, setName] = useState("");
+  let navigate = useNavigate();
+
+  const handleChange= e => {
+    setName(e.target.value)
+  }
+
+  const handleSubmit = async e => {
+    e.preventDefault();
+    const category = { name: name }
+    const headers = {
+      "Accept": "application/json",
+      "Content-Type": "application/json"
+    }
+    const options = {
+      method: "POST",
+      headers,
+      body: JSON.stringify(category)
+    }
+    const resp = await fetch("http://localhost:9292/categories", options)
+    const data = await resp.json()
+    setName(data)
+    
+  }
+    
   return (
     <div>
         <h1>New Category</h1>
-        <form>
+        <form onSubmit={handleSubmit}>
           <div>
             <label>Name: </label>
-            <input type="text"/>
+            <input type="text" value={name} onChange={handleChange}/>
           </div>
-          <input type="submit" value="Submit"/>
+          <br/>
+          <input type="submit" value="Submit" style={{backgroundColor: "lightcyan"}}/>
         </form>
     </div>
   )
