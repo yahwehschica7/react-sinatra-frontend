15d27ddebdc838bb9575d48444c5a3d1baf84c57
Jessica Nelson <yahwehschica@yahoo.com>
2022-09-06 11:13:49 -0400

    Create CategoryLink component to link to category names

---
+++ src/components/CategoryLink.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import { Link } from "react-router-dom"
+
+const CategoryLink = ({category}) => {
+  return (
+    <div>
+        <Link to={`http://localhost:9292/categories/${category.id}`}>
+            <h3>{category.name}</h3>
+        </Link>
+    </div>
+  )
+}
+
+export default CategoryLink
+
+//   return (
+//     <div>
+//       <br></br>
+//         <ul key={category.id} > 
+//           <h1>{category.name}</h1>
+//           <li key={category.id}> 
+//             {category.books.map((book) => {
+//               return (
+//                 <>
+//               <h2>{book.title}</h2>
+//               <h3>By: {book.author}</h3>
+//               <h4>My Thoughts: "{book.comment}"</h4>
+//               </>
+//             )})}
+//           </li>
+//       <br></br>              
+//         </ul> 
+//           <button style={{backgroundColor: "lightcyan"}}> 
+//               Edit Category
+//             </button>
+//             <button style={{backgroundColor: "lightcyan"}}> 
+//             Delete Category
+//             </button>
+//             <NavLink to={`/categories/${category.id}`}> 
+//               <button  style={{backgroundColor: "lightcyan"}}>
+//               Add a Book
+//               </button>
+//             </ NavLink>
+//             <NavLink to={`/books/${category.books.id}`}>
+//             <button style={{backgroundColor: "lightcyan"}}> 
+//               Delete a Book
+//             </button>
+//             </ NavLink>
+//       <br></br>
+//     </div>
+//   ) 
+// })
--- src/components/CategoryList.js
+++ src/components/CategoryList.js
@@ -1,5 +1,5 @@
 import React, {useEffect, useState} from 'react'
-
+import CategoryLink from "./CategoryLink"
 
 
 const CategoryList = () => {
@@ -26,45 +26,6 @@ const CategoryList = () => {
 export default CategoryList
 
 
-// displays category list. click on a category to see all the books. click on a book to see its details. 
 
-// const listOfCategories = categories.map(category => { 
 
    
-//   return (
-//     <div>
-//       <br></br>
-//         <ul key={category.id} > 
-//           <h1>{category.name}</h1>
-//           <li key={category.id}> 
-//             {category.books.map((book) => {
-//               return (
-//                 <>
-//               <h2>{book.title}</h2>
-//               <h3>By: {book.author}</h3>
-//               <h4>My Thoughts: "{book.comment}"</h4>
-//               </>
-//             )})}
-//           </li>
-//       <br></br>              
-//         </ul> 
-//           <button style={{backgroundColor: "lightcyan"}}> 
-//               Edit Category
-//             </button>
-//             <button style={{backgroundColor: "lightcyan"}}> 
-//             Delete Category
-//             </button>
-//             <NavLink to={`/categories/${category.id}`}> 
-//               <button  style={{backgroundColor: "lightcyan"}}>
-//               Add a Book
-//               </button>
-//             </ NavLink>
-//             <NavLink to={`/books/${category.books.id}`}>
-//             <button style={{backgroundColor: "lightcyan"}}> 
-//               Delete a Book
-//             </button>
-//             </ NavLink>
-//       <br></br>
-//     </div>
-//   ) 
-// })
